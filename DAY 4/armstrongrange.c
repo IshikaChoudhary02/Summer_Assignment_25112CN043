27ae90283a9472a1f991378beadbfcad9e974161
Ishika <ishikachoudhary0212@gmail.com>
2026-06-05 18:53:04 +0530

    Update armstrongrange.c

--- DAY 4/armstrongrange.c
+++ DAY 4/armstrongrange.c
@@ -1,25 +1,35 @@
 #include<stdio.h>
+#include<math.h>
+
 int main()
 {
-    int num, start, end, temp, rem, sum = 0;
+    int num, temp, digit, count = 0;
+    int sum = 0;
 
-    printf("Enter start: ");
-    scanf("%d", &start);
-    printf("Enter end: ");
-    scanf("%d", &end);
+    printf("Enter a number: ");
+    scanf("%d", &num);
 
-    for (num = start; num <= end; num++)
+    temp = num;
+
+    while(temp > 0)
     {
-        temp = num;
-        sum = 0;
-        while (temp > 0)
-        {
-            rem = temp % 10;
-            sum = sum + (rem * rem * rem);
-            temp = temp / 10;
-        }
-        if (sum == num)
-            printf("%d ", num);
+        count++;
+        temp = temp / 10;
     }
-return 0;
+
+    temp = num;
+
+    while(temp > 0)
+    {
+        digit = temp % 10;
+        sum = sum + pow(digit, count);
+        temp = temp / 10;
+    }
+
+    if(sum == num)
+        printf("Armstrong Number");
+    else
+        printf("Not an Armstrong Number");
+
+    return 0;
 }
