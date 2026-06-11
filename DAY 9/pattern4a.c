2496894720b6c42a670233df1c2f692f8e4780d7
Ishika <ishikachoudhary0212@gmail.com>
2026-06-11 16:57:46 +0530

    Update pattern4a.c

--- DAY 9/pattern4a.c
+++ DAY 9/pattern4a.c
@@ -7,11 +7,11 @@ int main()
     printf("Enter the number of rows: ");
     scanf("%d", &rows);
 
-    for (i = rows; i >= 1; i--) 
+    for (i = 1; i <= rows; i++) 
     {
-        for (j = 1; j <= i; j++) 
+        for (j = 1; j <= rows+1-i; j++) 
         {
-            printf("*");
+            printf("* ");
         }
         printf("\n");
     }
