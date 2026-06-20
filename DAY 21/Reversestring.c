775e74bdce1e77b2d05f00cf51bf6901b783442a
Ishika <ishikachoudhary0212@gmail.com>
2026-06-20 07:53:56 +0530

    Add files via upload

---
+++ DAY 21/Reversestring.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], rev[100];
+    int i, length = 0;
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);
+
+    while (str[length] != '\0' && str[length] != '\n') {
+        length++;
+    }
+
+    for (i = 0; i < length; i++) {
+        rev[i] = str[length - 1 - i];
+    }
+
+    rev[i] = '\0';
+
+    printf("Reversed String: %s\n", rev);
+
+    return 0;
+}
